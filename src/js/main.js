9d324f167d5951f8579d63f34ea68a7314620f8f
Ihor Tkachenko <Igor.tt6@gmail.com>
2023-02-19 16:06:09 +0200

    phone and text validation done

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkNumInputs from "./modules/checkNumInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -9,6 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
-
-
+    mask('[name="phone"]');
+    checkNumInputs('[name="name"]');
+    checkNumInputs('[name="message"]');
 })
